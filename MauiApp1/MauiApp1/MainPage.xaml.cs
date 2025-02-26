697ac5f0922ec3a5efe4559d24800b992de93d11
Aluno falso <aluno.falso@escola.falsa.com>
2025-02-25 22:26:20 -0300

    Adicionando tela 2

--- MauiApp1/MauiApp1/MainPage.xaml.cs
+++ MauiApp1/MauiApp1/MainPage.xaml.cs
@@ -20,6 +20,12 @@
 
             SemanticScreenReader.Announce(CounterBtn.Text);
         }
+
+        private void btnTela2_Cliked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new NewPage1());
+            Navigation.PopAsync(new btnVoltar());
+        }
     }
 
 }
